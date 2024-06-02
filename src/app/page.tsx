608c4f04bcaec1f6d4be37232eec9977e14f0309
Joseph Burdick <josephdburdick@gmail.com>
2024-06-01 21:16:15 -0400

    Initial commit to add mobile/desktop views

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,107 @@
+"use client";
+import { Avatar } from "@/components/ui/avatar";
+import { DeviceProvider, useDevice } from "@/lib/providers/DeviceContext";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/j0e/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/j0e/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div className="flex min-h-screen flex-col items-center justify-between p-4 md:p-8 lg:p-16 xl:p-24">
+      <DeviceProvider>
+        <Content />{" "}
+      </DeviceProvider>
+    </div>
   );
+}
+
+function MobileView() {
+  const { orientation } = useDevice();
+  return (
+    <div
+      className={cn(
+        orientation === "portrait" ? "bg-green-100" : "bg-red-100",
+        "flex flex-1 w-full container",
+      )}
+    >
+      <div className="grid md:grid-cols-12 gap-4">
+        <aside className="flex flex-col flex-1 lg:col-span-4 gap-4 lg:gap-16">
+          <section className="flex flex-col flex-1 gap-4 lg:gap-8">
+            <div className="flex gap-4 items-center">
+              <Avatar className="relative rounded-full w-24 h-24 flex items-center justify-center bg-gray-100">
+                <Image
+                  src="/j0e/assets/images/profile.png"
+                  width={96}
+                  height={96}
+                  alt="Profile Picture"
+                  className="rounded-full absolute w-full h-full object-cover bottom-0"
+                />
+              </Avatar>
+              <div>
+                <h1 className="text-2xl font-extralight">Joe Burdick</h1>
+                <h2 className="text-lg text-muted font-light text-accent">
+                  Web Software Engineer
+                </h2>
+              </div>
+            </div>
+          </section>
+          {/* <section> */}
+          {/*   <p> */}
+          {/*     With over 2 decades of experience, I assist companies to grow */}
+          {/*     their product with users in mind through strategic and creative */}
+          {/*     solutions. */}
+          {/*   </p> */}
+          {/*   <p>Reach out at josephdburdick@gmail.com</p> */}
+          {/* </section> */}
+        </aside>
+        <main className="md:col-span-8"></main>
+      </div>
+    </div>
+  );
+}
+
+function DesktopView() {
+  return (
+    <div className="flex flex-1 w-full container">
+      <div className="grid md:grid-cols-12 gap-4">
+        <aside className="flex flex-col flex-1 lg:col-span-4 gap-4 lg:gap-16">
+          <section className="flex flex-col flex-1 gap-4 lg:gap-8">
+            <div className="flex gap-4 items-center">
+              <Avatar className="relative rounded-full w-24 h-24 flex items-center justify-center bg-gray-100">
+                <Image
+                  src="/j0e/assets/images/profile.png"
+                  width={96}
+                  height={96}
+                  alt="Profile Picture"
+                  className="rounded-full absolute w-full h-full object-cover bottom-0"
+                />
+              </Avatar>
+              <div>
+                <h1 className="text-2xl font-extralight">Joe Burdick</h1>
+                <h2 className="text-lg text-muted font-light text-accent">
+                  Web Software Engineer
+                </h2>
+              </div>
+            </div>
+          </section>
+          <section>
+            <p>
+              With over 2 decades of experience, I assist companies to grow
+              their product with users in mind through strategic and creative
+              solutions.
+            </p>
+            <p>Reach out at josephdburdick@gmail.com</p>
+          </section>
+        </aside>
+        <main className="md:col-span-8">
+          {Array.from({ length: 10 }).map((_, i) => (
+            <div key={i}>{i}</div>
+          ))}
+        </main>
+      </div>
+    </div>
+  );
+}
+
+function Content() {
+  const { isMobile } = useDevice();
+  return isMobile ? <MobileView /> : <DesktopView />;
 }
