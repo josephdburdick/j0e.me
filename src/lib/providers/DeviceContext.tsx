f7d3b1dbccce61b373bca8143225af98f7755915
Joseph Burdick <josephdburdick@gmail.com>
2024-06-02 01:20:03 -0400

    Add async to handleResize and attempt to scroll

--- src/lib/providers/DeviceContext.tsx
+++ src/lib/providers/DeviceContext.tsx
@@ -19,20 +19,27 @@ const DeviceProvider: React.FC<{ children: React.ReactNode }> = ({
   useEffect(() => {
     if (typeof window === "undefined") return
     function handleResize() {
-      setIsMobile(/Mobi|Android/i.test(navigator.userAgent))
-      if (/Mobi|Android/i.test(navigator.userAgent)) {
+      const mobileTest =
+        /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i
+      setIsMobile(mobileTest.test(navigator.userAgent))
+
+      if (mobileTest.test(navigator.userAgent)) {
         if (window?.matchMedia("(orientation: portrait)").matches) {
           setOrientation("portrait")
         } else if (window.matchMedia("(orientation: landscape)").matches) {
           setOrientation("landscape")
         }
+
+        window.scrollTo(0, 0)
       } else {
         setOrientation(null)
       }
     }
 
     window.addEventListener("orientationchange", handleResize)
-    setTimeout(() => handleResize(), 0)
+
+    setTimeout(handleResize, 0)
+
     return () => {
       window.removeEventListener("orientationchange", handleResize)
     }
